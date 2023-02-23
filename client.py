ac081293e5fea9151f1bd1d981d97b2bf942c520
Simbarashe Mawere <20simbarashem@gmail.com>
2023-02-23 15:26:39 +0200

    Simba's experiments

---
+++ client.py
@@ -0,0 +1,45 @@
+import os
+from socket import *
+
+serverName = "localhost"
+serverPort = 12000
+
+while True:
+    clientSocket = socket(AF_INET, SOCK_STREAM)
+    clientSocket.connect((serverName,serverPort))
+
+    filename = input("Enter the name of the file you want to search for:\n")
+    clientSocket.send(filename.encode())
+
+    confirmation = clientSocket.recv(1024)
+    return_message = confirmation.decode()
+    print(return_message)
+    clientSocket.close()
+
+    if ("not" not in return_message):
+        down = input("Do you want to download the file (y/n)\n")
+        if down:
+            clientSocket = socket(AF_INET, SOCK_STREAM)
+            clientSocket.connect((serverName,serverPort))
+
+            message = "download " + filename 
+            clientSocket.send(message.encode())
+            
+
+            if not os.path.isdir("./downloads"): os.mkdir("./downloads")
+            # implement getting file from the server
+            file = open(f"./downloads/{filename}", "w")
+
+            data = clientSocket.recv(1024).decode("utf-8")
+            file.write(data)
+
+            file.close()
+
+            print("File received!")
+
+            clientSocket.close()
+            
+            
+        
+    
+    # x = input("buffer")
---
+++ server.py
@@ -0,0 +1,34 @@
+import os
+from socket import *
+
+serverPort = 12000
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket.bind(("",serverPort))
+serverSocket.listen(1)
+print("The server is ready to receive.\n")
+
+while True:
+    connectSocket, addr = serverSocket.accept()
+    message = connectSocket.recv(1024).decode()
+    if message.find("download") == 0:
+        directive, filename = message.split(" ")
+        print(f"downloading file: {filename}")
+        # implement sending the file onto the socket
+        print(f"Sending ... {filename}")
+
+        file = open(f"./serverfiles/{filename}", "r")
+        data = file.read()
+
+        connectSocket.send(data.encode("utf-8"))
+        file.close()
+
+        print("File sent!")
+    
+    else:
+        print(f"Request received for file with filename: {message}")
+        x = os.path.isfile(f"./serverfiles/{message}")
+
+        return_message = "File found" if x else "File not found"
+        connectSocket.send(return_message.encode())
+
+    connectSocket.close()
